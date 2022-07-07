9a0628cb2e1ff6e40b2b7f17a683639f9e787276
Claudia Lisboa <ccalumby@gmail.com>
2022-07-06 22:49:12 -0400

    first commit

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+// URL de base du serveur
+const BASE_SERVEUR = '';
+
+// Config de la BD
+const BD_HOTE = 'localhost';
+const BD_UTIL = 'root';
+const BD_MDP = 'root';
+const BD_NOM = 'magasingeneral';
